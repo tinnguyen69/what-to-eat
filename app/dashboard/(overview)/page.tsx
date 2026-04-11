6b3516c07683e5c0231939b7d86b7fdf01774af2
tinnguyen69 <25825527+tinnguyen69@users.noreply.github.com>
2026-04-11 12:45:49 +0700

    Finished Dashboard course.
    * Use nestjs dashboard template
    
    * Finished Chapter 10
    
    * Fix merge from main
    
    * Remove recreated files due to merge
    
    * Finished Dashboard course.
    
    ---------
    
    Co-authored-by: TínNguyễn <tinnd2@vnext.vn>

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,7 +7,12 @@ import {
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
 } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
 import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page() {
   return (
@@ -15,12 +20,12 @@ export default async function Page() {
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
-      <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
       </div>
-      <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
